1843e079a4a0978f9b8067e23fdb7c7518102754
Sasi Kiran <sasikiran0601@gmail.com>
2026-05-23 00:42:18 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,15 @@
 from fastapi.testclient import TestClient
+
 from app.main import app
 
 client = TestClient(app)
+
 
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Sasikiran"}
+
 
 def test_health():
     response = client.get("/health")
